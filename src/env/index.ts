7cb7b48a52c95f73620df53437d02bb4a1c8ccb3
Vicente Sanchez <vikom.sanchez@gmail.com>
2025-02-02 20:53:06 -0300

    feat: add database initial setup

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,6 +3,7 @@ import z from 'zod'
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   BASE_URL: z.string().url(),
+  DATABASE_URL: z.string().url(),
   NODE_ENV: z
     .enum(['development', 'production', 'test'])
     .default('development'),
