eb152059b15fa55d3afcfc6fcad24bf64ba1b98a
Vicente Sanchez <vikom.sanchez@gmail.com>
2025-02-02 20:45:07 -0300

    initial commit

---
+++ src/env/index.ts
@@ -0,0 +1,12 @@
+import z from 'zod'
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
+  BASE_URL: z.string().url(),
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'),
+  HOST: z.string().default('0.0.0.0'),
+})
+
+export const env = envSchema.parse(process.env)
---
+++ src/http/server.ts
@@ -0,0 +1,66 @@
+import fastifySwagger from '@fastify/swagger'
+import fastify from 'fastify'
+import type { FastifyInstance } from 'fastify/types/instance'
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+
+import { env } from '@/env'
+
+import { errorHandler } from './error-handler'
+import { transformSwaggerSchema } from './transform-schema'
+
+const app: FastifyInstance = buildFastifyInstance()
+
+export function startServer() {
+  app.setValidatorCompiler(validatorCompiler)
+  app.setSerializerCompiler(serializerCompiler)
+
+  app.register(fastifySwagger, {
+    openapi: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Boilerplate',
+        version: '0.1.0',
+      },
+      servers: [
+        {
+          url: env.BASE_URL,
+          description: 'Development server',
+        },
+      ],
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+          },
+        },
+      },
+    },
+    transform: (schema) => {
+      try {
+        return transformSwaggerSchema(schema)
+      } catch (err) {
+        return schema
+      }
+    },
+  })
+
+  app.setErrorHandler(errorHandler)
+
+  app
+    .listen({
+      port: env.PORT,
+      host: '0.0.0.0',
+    })
+    .then(() => {
+      console.info(`HTTP server running at ${env.BASE_URL}`)
+    })
+}
+
+export function buildFastifyInstance() {
+  return fastify()
+}
